5f1e3c050f058795afa0dc2efa97766bb2caf7bd
igrmaurat <igrmaurat@mail.ru>
2018-06-04 15:50:46 +0300

    Переписал client_read в виде класса.
    Удалил les_2_read_client - вместо него client_read
    
    В следующем релизе:
    
    1. Переписать write_client в виде класса.
    2. Добавить класс для сообщения.
    3. Добавить mock заглущку для тестирования подключения клиента.
    4. Добавить тест для подключения клиента к серверу.

---
+++ read_client.py
@@ -0,0 +1,25 @@
+# python read_client.py
+# Пока работает с сервером lesson_2_server.py
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+ADDRESS = ('localhost', 9888)
+
+
+class ClientRead:
+    def __init__(self, ADDRESS):
+        self.ADDRESS = ADDRESS
+
+    def echo_client(self):
+        with socket(AF_INET, SOCK_STREAM) as sock:  # Создать сокет TCP
+            sock.connect(self.ADDRESS)  # Соединиться с сервером
+            while True:
+                # Получаем ответ сервера, размер не больше 1024 байт
+                data_from_server = sock.recv(1024).decode('ascii')
+                print(data_from_server)
+
+
+if __name__ == '__main__':
+    client = ClientRead(ADDRESS)
+    client.echo_client()
